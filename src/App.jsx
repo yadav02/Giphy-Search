67cf311bef33a922eb10c07f4bd13092acb53c4f
yadav02 <satyendras767@gmail.com>
2021-04-23 09:53:19 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import Giphy from "./components/Giphy";
+
 import "./App.css";
+import UserData from "./components/userData";
 const App = () => {
   return (
+    <Provider store={store}>
     <div>
       <Giphy />
+      <UserData/>
     </div>
+    </Provider>
   );
 };
 
